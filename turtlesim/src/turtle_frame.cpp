2e206f37c231968ba0159201fc4836b395604f32
zinzuvadiadev <zinzuvadiadev08@gmail.com>
2025-02-23 14:51:25 +0530

    Updated Task 4,5 and 6

--- turtlesim/src/turtle_frame.cpp
+++ turtlesim/src/turtle_frame.cpp
@@ -53,12 +53,12 @@ namespace turtlesim
 
 TurtleFrame::TurtleFrame(rclcpp::Node::SharedPtr & node_handle, QWidget * parent, Qt::WindowFlags f)
 : QFrame(parent, f)
-  , path_image_(500, 500, QImage::Format_ARGB32)
+  , path_image_(1920, 1080, QImage::Format_ARGB32)
   , path_painter_(&path_image_)
   , frame_count_(0)
   , id_counter_(0)
 {
-  setFixedSize(500, 500);
+  setFixedSize(1920, 1080);
   setWindowTitle("TurtleSim");
 
   srand(time(NULL));
